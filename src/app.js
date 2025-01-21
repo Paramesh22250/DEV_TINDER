0cd694dd4811c06917c18754731f831043c26633
Paramesh22250 <parameshwarareddy22250@gmail.com>
2025-01-21 13:28:07 +0530

    firstcommit to nodeDEVTINDER

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+
+// app.use("/hello/user",(req,res)=>{
+//     res.status(200)
+//     res.send("Hello from user!!")
+// })
+// app.use("/hello",(req,res)=>{
+//     res.status(200)
+//     res.send("Hello from hello!!")
+// })
+
+app.use("/user",(req,res,next)=>{
+    console.log("1st response")
+    next()
+},(req,res,next)=>{
+    console.log("2nd response")
+    next()
+},(req,res)=>{
+    console.log("3rd responses")
+    res.send("3-response sent")
+}
+)
+app.listen(3000,()=>{
+    console.log("listening to port 3000")
+})
