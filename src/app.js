7952c69904ee4ec5d2a3e7adae113452d90a3b1c
Paramesh22250 <parameshwarareddy22250@gmail.com>
2025-01-27 10:41:59 +0530

    patch

--- src/app.js
+++ src/app.js
@@ -14,6 +14,40 @@ app.post('/signup',async(req,res)=>{
         res.send("Error in saving data")
     }
 })
+app.get("/feed",async(req,res)=>{
+    try{
+        const user = await UserModel.find();
+        if(!user){
+            res.send("No user found")
+        }else{
+            res.send(user)
+        }
+        
+    }catch(e){
+        res.send("Something went wrong")
+    }
+})
+app.get("/user",async (req,res) => {
+    try {
+        const user = await UserModel.find({email:"Alice@gmail.coms"})
+        if(user.length<1){
+            res.send("No user found!!")
+        }else{
+            res.send(user)
+        }
+    } catch (error) {
+        res.send("Something went wrong")
+    }
+})
+
+app.patch("/user",async (req,res) => {
+    try {
+        await UserModel.findOneAndUpdate({email:"Alice@gmail.com"},{email:"IamAlice@gmail.com"})
+        res.send("Upadated successfully")
+    } catch (error) {
+        res.send("Something went wrong")
+    }
+})
 
 
 
